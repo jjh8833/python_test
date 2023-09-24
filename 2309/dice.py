ecb7ead14011272759bc3e991dfbb4d16fcd0841
roidproj1 <roidproj1@gmail.com>
2023-09-24 16:45:34 +0900

    Initial commit

---
+++ 2309/dice.py
@@ -0,0 +1,9 @@
+import random
+coin=['앞면','뒷면']
+j = 0
+for i in range(1,11):
+    j += i
+    print(random.choice(coin))
+
+
+
---
+++ 2309/gugudan.py
@@ -0,0 +1,11 @@
+
+ 
+i = 9
+
+ 
+while(i>0):
+    j = 9 
+    while(j>0):
+        print("{} * {} = {}".format(i, j, i*j))
+        j-=1
+    i-=1
